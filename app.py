95fde6ab5a9863447e922381432440c7c7d765e5
Mauricio200101 <81936691+Mauricio200101@users.noreply.github.com>
2026-07-16 17:22:36 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -351,8 +351,8 @@ def actualizar_stock_sheet(id_insumo, nuevo_stock, nombre_insumo, tipo_mov, cant
                     int(dias)
                 ]
                 hoja_alquileres.append_row(fila_registro)
-    else:
-        st.error("No se encontró el ID del insumo en la hoja de cálculo.")
+                else:
+                    st.error("No se encontró el ID del insumo en la hoja de cálculo.")
     except Exception as e:
         st.error(f"Error al conectar con la base de datos: {e}")
 
