6a4e7ffb6840da1eca17c08af866d2a5187c2ca3
Mauricio200101 <81936691+Mauricio200101@users.noreply.github.com>
2026-07-16 16:47:32 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -549,25 +549,25 @@ with tab_operaciones:
                             # Estas columnas SOLO se crean si el motivo es Alquiler
                             col_al_dest1, col_al_dest2 = st.columns(2)
             
-                        with col_al_dest1:
-                            empresa_destino = st.selectbox("🏢 Empresa:", empresas_disponibles)
-                            agencias_filtradas = [ag for ag in agencias_disponibles if ag.startswith(empresa_destino.strip())]
-                            agencia_destino = st.selectbox(
-                            "🏢 Agencia:", 
-                            agencias_filtradas, 
-                            format_func=lambda x: x.replace(empresa_destino + " - ", "")
-                            )
+                            with col_al_dest1:
+                                empresa_destino = st.selectbox("🏢 Empresa:", empresas_disponibles)
+                                agencias_filtradas = [ag for ag in agencias_disponibles if ag.startswith(empresa_destino.strip())]
+                                agencia_destino = st.selectbox(
+                                "🏢 Agencia:", 
+                                agencias_filtradas, 
+                                format_func=lambda x: x.replace(empresa_destino + " - ", "")
+                                )
 
-                        with col_al_dest2:
-                        areas_filtradas = [ar for ar in areas_disponibles if ar.startswith(agencia_destino)]
-                        area_o_precio_destino = st.selectbox(
-                        "📍 Área:", 
-                        areas_filtradas, 
-                        format_func=lambda x: x.replace(agencia_destino + " - ", "")
-                        )
-            
-                        st.markdown("---")
-                        st.markdown("📊 **Control de Contadores (Alquiler)**")
+                            with col_al_dest2:
+                                areas_filtradas = [ar for ar in areas_disponibles if ar.startswith(agencia_destino)]
+                                area_o_precio_destino = st.selectbox(
+                                "📍 Área:", 
+                                areas_filtradas, 
+                                format_func=lambda x: x.replace(agencia_destino + " - ", "")
+                                )
+                
+                            st.markdown("---")
+                            st.markdown("📊 **Control de Contadores (Alquiler)**")
             
             # Solo buscar el historial si el motivo es Alquiler
             ultimo_registro_alq = obtener_ultimo_alquiler(seleccionado, empresa_destino, area_o_precio_destino, agencia_destino)
