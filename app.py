d30d273c00f8e633b548f0b887e3516b6f3c8081
Mauricio200101 <81936691+Mauricio200101@users.noreply.github.com>
2026-07-28 18:41:21 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -948,7 +948,7 @@ else:
 # ==========================================
 # 2. PESTAÑA DE VALORIZACIÓN DEL INVENTARIO
 # ==========================================
-if seccion == "Valorización del Inventario":
+if st.session_state["menu_activo"] == "Valorización del Inventario":
     if st.session_state["rol_actual"] == "Administrador":
         st.subheader("💰 Resumen Monetario del Inventario")
         if not df_insumos.empty:
